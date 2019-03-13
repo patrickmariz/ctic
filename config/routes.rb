3ac3a7a40d7e7c0b43e2b6d3ab864ec29545d6ff
pam4 <pam4@cin.ufpe.br>
2019-03-13 23:46:34 +0000

    faltando apenas mailsender

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :servicos
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   devise_for :users
   root to: 'home#index' 
   resources :agendamentos
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
